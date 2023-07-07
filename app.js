02e7f23563d5cb3fd4fd1c1f29073f926ddbb309
jiyongnoh <njy99000@naver.com>
2023-07-07 15:30:48 +0900

    feat: 실시간 채팅 서버

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const port = 3001;
+const cors = require("cors");
+// cors error 해결
+app.use(cors());
+// json parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Bye World!");
+});
+
+app.get("/user", (req, res) => {
+  res.json({ msg: "User Hello!!" });
+});
+
+app.post("/user", cors(), (req, res) => {
+  const message = req.body;
+  // emit: 소켓에 데이터를 보내는 메서드. (eventName, data)
+  // res.socket.server.io.emit("message", message);
+  res.status(201).json(message.msg);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
